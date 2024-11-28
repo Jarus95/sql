ac63a2937270f07fdcf35915b3a7deb40bbf1f91
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2024-11-28 14:51:20 +0500

    Update ready.sql

--- ready.sql
+++ ready.sql
@@ -1,3 +1,5 @@
+SELECT DISTINCT Country FROM Customers;
+
 SELECT product_id, product_name, supplier_id, category_id, quantity_per_unit, unit_price, units_in_stock, units_on_order, reorder_level, discontinued
 	FROM public.products;
 
